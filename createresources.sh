457e602dc5cd220054d7ae437a39c07ca21a75b5
anasasa <43885172+anasasa@users.noreply.github.com>
2022-02-05 15:17:10 +0100

    Update createresources.sh

--- createresources.sh
+++ createresources.sh
@@ -4,13 +4,13 @@ set -o errexit
 set -o pipefail
 DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
 echo "Creating Azure resources"
-az group create --name "group${myindex}" --location australiaeast
+az group create --name "group${MY_INDEX}" --location australiaeast
 echo "Resource group created"
 {
-	az deployment group create --name "hdi${myindex}" --resource-group "group${myindex}" --template-file ${DIR}/HDI/hdi_template.json --parameters ${DIR}/HDI/hdi_parameters.json -p clusterName="hdi${myindex}" -p clusterLoginPassword="${hdi_password}" -p sshPassword="${hdi_password}"
+	az deployment group create --name "hdi${MY_INDEX}" --resource-group "group${MY_INDEX}" --template-file ${DIR}/HDI/hdi_template.json --parameters ${DIR}/HDI/hdi_parameters.json -p clusterName="hdi${MY_INDEX}" -p clusterLoginPassword="${HDI_PASSWORD}" -p sshPassword="${HDI_PASSWORD}"
 	echo "Hadoop cluster created"
 }&
-az storage account create --name "func${myindex}storage" --sku Standard_LRS --location australiaeast --resource-group "group${myindex}"
+az storage account create --name "func${MY_INDEX}storage" --sku Standard_LRS --location australiaeast --resource-group "group${MY_INDEX}"
 echo "Storage account for function created"
-az functionapp create --consumption-plan-location australiaeast --runtime python --runtime-version 3.8 --functions-version 3 --os-type linux --resource-group "group${myindex}" --storage-account "func${myindex}storage" --name "func${myindex}"
+az functionapp create --consumption-plan-location australiaeast --runtime python --runtime-version 3.8 --functions-version 3 --os-type linux --resource-group "group${MY_INDEX}" --storage-account "func${MY_INDEX}storage" --name "func${MY_INDEX}"
 echo "Function created"
