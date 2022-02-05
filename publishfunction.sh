c99a61c73ce7f45818c62a7a88283f5ccb11a8e0
anasasa <43885172+anasasa@users.noreply.github.com>
2022-02-05 15:18:20 +0100

    Update publishfunction.sh

--- publishfunction.sh
+++ publishfunction.sh
@@ -3,6 +3,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 cd ./UiPDdAwC/Function
-sed -i "11s/.*/    client.connect('hdi${myindex}-ssh.azurehdinsight.net', username='sshuser', password='${hdi_password}')/" ./read/__init__.py
-func azure functionapp publish func69303
+sed -i "11s/.*/    client.connect('hdi${MY_INDEX}-ssh.azurehdinsight.net', username='sshuser', password='${HDI_PASSWORD}')/" ./read/__init__.py
+func azure functionapp publish "func${MY_INDEX}"
 cd ../..
